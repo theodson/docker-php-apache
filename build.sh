1c89f8e68d9b2fb2781bb2d91ce8166f25619439
Theodson <theodson@gmail.com>
2017-11-01 01:07:53 +1100

    update git from 1.7 to 2.5

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-docker build php-54  -t 'theodson/php:5.4'
-docker build php-53  -t 'theodson/php:5.3'
+docker build php-54  -t 'theodson/php:5.4' $@
+docker build php-53  -t 'theodson/php:5.3' $@
+docker build apache-php-53  -t 'theodson/apache-php:5.3' $@
+docker build apache-php-54  -t 'theodson/apache-php:5.4' $@
